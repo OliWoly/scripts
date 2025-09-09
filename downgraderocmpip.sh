9be3a134cbc4bf1d9067bb6b3adb309b342401a5
Oliwier Kulczycki <oleknuso282@gmail.com>
2025-09-09 12:41:41 +0100

    backup

---
+++ downgraderocmpip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Stuff like this is how you download from other wheels.
+
+
+
+#pip install torch==2.3.1+rocm6.0 torchvision==0.18.1+rocm6.0 torchaudio==2.3.1+rocm6.0 --index-url https://download.pytorch.org/whl/rocm6.0
+
